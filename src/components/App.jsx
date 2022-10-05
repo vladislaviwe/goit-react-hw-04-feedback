9d8dcd8e69ea9f45b04bea6573f128c43f845eee
Vladislav Klymenko <vladislaviwe@outlook.com>
2022-10-05 13:43:52 +0300

    first task completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,40 @@
-import { Component } from "react";
 import Section from "./Section";
 import FeedbackActions from "./FeedbackActions";
 import FeedbackResults from "./FeedbackResults";
 import { Container } from './FeedbackStyled';
+import { useState } from "react";
 
-class App extends Component {
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  }
 
-  handleButtonClick = (e) => {
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleButtonClick = (e) => {
     const name = e.target.name;
-    
-    this.setState(prevState => ({
-      [name]: prevState[name] + 1,
-    }))
+
+    switch (name) {
+      case 'good':
+        setGood((prev) => prev + 1)
+        return;
+      case 'neutral':
+        setNeutral((prev) => prev + 1)
+        return;
+      case 'bad':
+        setBad((prev) => prev + 1)
+        return;  
+      default:
+        return;
+    }
   }
 
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
-
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   }
 
-  countPositiveFeedbackPercentage() {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
     const result = (good / total) * 100;
 
     if(!total) {
@@ -38,14 +44,11 @@ class App extends Component {
     return Number(result.toFixed(2));
   }
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const { handleButtonClick } = this;
-    const total = this.countTotalFeedback();
-    const positiveFeedback = this.countPositiveFeedbackPercentage();
+  const total = countTotalFeedback();
+  const positiveFeedback = countPositiveFeedbackPercentage();
 
-    return (
-      <Container>
+  return (
+    <Container>
         <Section title="Please leave feedback">
           <FeedbackActions handleButtonClick={handleButtonClick}/>
         </Section>
@@ -53,10 +56,63 @@ class App extends Component {
         <Section title="Statistics">
           <FeedbackResults good={good} neutral={neutral} bad={bad} total={total} positiveFeedback={positiveFeedback}/>
         </Section>
-      </Container>
-    );
-  }
+    </Container>
+  )
 }
 
-export default App;
+
+// class App extends Component {
+
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0
+//   }
+
+//   handleButtonClick = (e) => {
+//     const name = e.target.name;
+    
+//     this.setState(prevState => ({
+//       [name]: prevState[name] + 1,
+//     }))
+//   }
+
+//   countTotalFeedback() {
+//     const { good, neutral, bad } = this.state;
+
+//     return good + neutral + bad;
+//   }
+
+//   countPositiveFeedbackPercentage() {
+//     const { good } = this.state;
+//     const total = this.countTotalFeedback();
+//     const result = (good / total) * 100;
+
+//     if(!total) {
+//       return 0;
+//     }
+    
+//     return Number(result.toFixed(2));
+//   }
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const { handleButtonClick } = this;
+//     const total = this.countTotalFeedback();
+//     const positiveFeedback = this.countPositiveFeedbackPercentage();
+
+//     return (
+//       <Container>
+//         <Section title="Please leave feedback">
+//           <FeedbackActions handleButtonClick={handleButtonClick}/>
+//         </Section>
+
+//         <Section title="Statistics">
+//           <FeedbackResults good={good} neutral={neutral} bad={bad} total={total} positiveFeedback={positiveFeedback}/>
+//         </Section>
+//       </Container>
+//     );
+//   }
+// }
+
 
